31786e2d443cacc704f218fa96e500230ced6675
mome44 <mome4401@gmail.com>
2025-07-11 20:58:55 +0200

    completed skeleton of baseline plate detection method

--- metodo_innovativo.py
+++ metodo_innovativo.py
@@ -1,53 +1,8 @@
 import os
 import torch
-import cv2
-from torchvision.transforms import functional as F
-
-
-class PlateDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, ann_dir, transforms=None):
-        self.img_dir = img_dir
-        self.ann_dir = ann_dir
-        self.transforms = transforms
-        self.imgs = sorted(os.listdir(img_dir))
-
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.imgs[idx])
-        ann_path = os.path.join(self.ann_dir, self.imgs[idx].replace('.jpg', '.txt'))
-
-        img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        boxes = []
-        labels = []
-
-        with open(ann_path) as f:
-            for line in f:
-                class_id, x1, y1, x2, y2 = map(int, line.strip().split())
-                boxes.append([x1, y1, x2, y2])
-                labels.append(class_id)
-
-        boxes = torch.tensor(boxes, dtype=torch.float32)
-        labels = torch.tensor(labels, dtype=torch.int64)
-        img_id = torch.tensor([idx])
-
-        target = {
-            "boxes": boxes,
-            "labels": labels,
-            "image_id": img_id
-        }
-
-        img = F.to_tensor(img)
-        return img, target
-
-    def __len__(self):
-        return len(self.imgs)
-
+import matplotlib.pyplot as plt
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, retinanet_resnet50_fpn
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, RetinaNetClassificationHead
-
-
-IOU_THRESHOLD = 0.5
 
 #Hyperparameters
 BATCH_SIZE = 4
@@ -56,25 +11,29 @@ MOMENTUM = 0.9
 WEIGHT_DECAY = 0.0005
 NUM_EPOCHS = 10
 
-def compute_iou(boxA, boxB):
-    # box format: [x1, y1, x2, y2]
-    xA = max(boxA[0], boxB[0])
-    yA = max(boxA[1], boxB[1])
-    xB = min(boxA[2], boxB[2])
-    yB = min(boxA[3], boxB[3])
+SAVE_NAME = f"n_epochs_{NUM_EPOCHS}_bs_{BATCH_SIZE}_LR_{LR}_Mom_{MOMENTUM}_wd_{WEIGHT_DECAY}"
 
-    interArea = max(0, xB - xA) * max(0, yB - yA)
-    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
-    boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
+def compute_iou(box_1, box_2):
+    #it is a metric that involves the intersection of the two areas
+    #over the union, and returns a matching percentage
+    
+    #coputing the coordinate of the intersections
+    x1 = max(box_1[0], box_2[0])
+    y1 = max(box_1[1], box_2[1])
+    x2 = min(box_1[2], box_2[2])
+    y2 = min(box_1[3], box_2[3])
 
-    iou = interArea / float(boxAArea + boxBArea - interArea + 1e-6)
+    interArea = max(0, x2 - x1) * max(0, y2 - y1)
+    boxAArea = (box_1[2] - box_1[0]) * (box_1[3] - box_1[1])
+    boxBArea = (box_2[2] - box_2[0]) * (box_2[3] - box_2[1])
+
+    iou = interArea / float(boxAArea + boxBArea - interArea + 1e-6)  #+1e-6 is used to avoid the division per zero
     return iou
 
-# Carica modello
+#loading the pretrained model
 model = fasterrcnn_resnet50_fpn(weights="DEFAULT")
 #model = retinanet_resnet50_fpn(weights="DEFAULT")
 
-# Modifica classificatore
 #Since we want that the last classifier layer to have only an output of two
 #so we get the input dimension of it
 in_features = model.roi_heads.box_predictor.cls_score.in_features
@@ -88,8 +47,9 @@ model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes=2)  #
 
 #cambiare il dataloader in modo che ce ne sia uno per train validation e test
 dataset = PlateDataset("dataset/images", "dataset/annotations")
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
-
+train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+val_dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+test_dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
 #this optimizer uses stochastic gradient descent and has in input the parameters (weights) from 
 #the pretrained model
 params = model.parameters()
@@ -99,12 +59,18 @@ optimizer = torch.optim.SGD(params, lr=LR, momentum=MOMENTUM, weight_decay=WEIGH
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
-#train we are doing fine tuning on the task of recognizing plate
+iou_scores_val=[]
+iou_scores_train=[]
+
+#TRAIN LOOP we are doing fine tuning on the task of recognizing plate
 for e in range(NUM_EPOCHS):
     model.train()
     train_loss= 0.0
+    val_loss = 0.0
+    train_iou = []
+    val_iou = []
     #does the for loop for all the items in the same batch
-    for images, labels in dataloader:
+    for images, labels in train_dataloader:
         #moves the images and labels to the GPU
         images = list(img.to(device) for img in images)
         labels = [{k: v.to(device) for k, v in t.items()} for t in labels]
@@ -122,72 +88,115 @@ for e in range(NUM_EPOCHS):
         optimizer.step()
 
         train_loss += total_step_loss.item()
+        #put the model in the evaluation phase to get the preditions
+        model.eval() 
+        with torch.no_grad():
+            train_outputs = model(images)
+        model.train()
+        for pred, target in zip(train_outputs, labels):
+                #prediction is a list that contains a score from 0 to 1 that 
+                #includes the confidence of the answer, so if this score is less than 0.5
+                #we will not consider it               
+                if len(pred['scores']) > 0:
+                    #we take the index of the detected plate with the best score
+                    best_index = pred['scores'].argmax()
+                    best_box = pred['boxes'][best_index]
 
-    avg_train_loss = train_loss / len(dataloader) #train_dataloader
+                    true_box = target['boxes'].cpu()[0]
 
-    print(f"Epoch {e+1}/{NUM_EPOCHS} - accuracyloss: {avg_train_loss.item():.4f}")
+                    single_iou = compute_iou(best_box.numpy(), true_box.numpy())
+                    train_iou.append(single_iou)
 
+    #compute the mean of the iou training score
+    mean_train_iou = sum(train_iou)/len(train_iou)
+    iou_scores_train.append(mean_train_iou)
+
+    #Validation phase
     model.eval()
-    val_loss = 0.0
-
-    TP = 0  # true positives
-    FP = 0  # false positives
-    FN = 0  # false negatives
-    iou_scores = []
-
     with torch.no_grad():
-        for images, targets in val_dataloader:
+        for images, labels in val_dataloader:
             images = [img.to(device) for img in images]
-            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            labels = [{k: v.to(device) for k, v in t.items()} for t in labels]
 
             # Calcolo della loss (solo per monitoraggio)
-            loss_dict = model(images, targets)
+            loss_dict = model(images, labels)
             loss = sum(loss for loss in loss_dict.values())
             val_loss += loss.item()
 
             # Inference
-            outputs = model(images)
+            val_outputs = model(images)
 
-            for pred, target in zip(outputs, targets):
-                pred_boxes = pred['boxes'].cpu()
+            for pred, target in zip(val_outputs, labels):
                 #prediction is a list that contains a score from 0 to 1 that 
                 #includes the confidence of the answer, so if this score is less than 0.5
-                #we will not consider it
-                pred_scores = pred['scores'].cpu()
-                true_boxes = target['boxes'].cpu()
-                 #selezionare la box che ha lo score più alto
-                best_index = pred['scores'].argmax()
-                best_box = pred['boxes'][best_index]
-                best_score = pred['scores'][best_index]
-               
-                matched = set()
-                for i, pbox in enumerate(pred_boxes):
-                    
-                    matched_flag = False
-                    for j, tbox in enumerate(true_boxes):
-                        iou = compute_iou(pbox.numpy(), tbox.numpy())
-                        if iou > IOU_THRESHOLD and j not in matched:
-                            TP += 1
-                            matched.add(j)
-                            iou_scores.append(iou)
-                            matched_flag = True
-                            break
-                    if not matched_flag:
-                        FP += 1
-                FN += len(true_boxes) - len(matched)
+                #we will not consider it               
+                if len(pred['scores']) > 0:
+                    #we take the index of the detected plate with the best score
+                    best_index = pred['scores'].argmax()
+                    best_box = pred['boxes'][best_index]
 
-    precision = TP / (TP + FP + 1e-6)
-    recall = TP / (TP + FN + 1e-6)
-    f1 = 2 * precision * recall / (precision + recall + 1e-6)
-    mean_iou = sum(iou_scores) / len(iou_scores) if iou_scores else 0.0
+                    true_box = target['boxes'].cpu()[0]
 
-    print(f"Epoch {epoch+1}")
-    print(f"Train Loss: {train_loss/len(train_dataloader):.4f}")
-    print(f"Val Loss: {val_loss/len(val_dataloader):.4f}")
-    print(f"Precision: {precision:.4f} | Recall: {recall:.4f} | F1: {f1:.4f} | mIoU: {mean_iou:.4f}")
+                    single_iou = compute_iou(best_box.numpy(), true_box.numpy())
+                    val_iou.append(single_iou)
 
-torch.save(model.state_dict(), "fasterrcnn_plate_detector.pth")
+    #compute the mean of the iou validation score
+    mean_val_iou = sum(val_iou)/len(val_iou)
+    iou_scores_val.append(mean_val_iou)
 
+    print(f"Epoch {e +1}/{NUM_EPOCHS} - train loss: {train_loss/len(train_dataloader):.4f} - val mIou: {mean_val_iou}" )
 
-model.load_state_dict(torch.load("fasterrcnn_plate_detector.pth"))
+#Saving the model
+torch.save(model.state_dict(), f"models/fasterrcnn_plate_detector-{SAVE_NAME}.pth")
+
+#Plotting the figure for the train and validation
+plt.figure(figsize=(8, 5))
+plt.plot(NUM_EPOCHS, iou_scores_train, label="train IoU", marker='o')
+plt.plot(NUM_EPOCHS, iou_scores_val, label="validation IoU", marker='s')
+plt.xlabel("epoch")
+plt.ylabel("IoU")
+plt.title("Train and validation iou per epoch")
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
+plt.savefig(f"imgs/train_validation-{SAVE_NAME}.png")
+
+model.load_state_dict(torch.load(f"models/fasterrcnn_plate_detector-{SAVE_NAME}.pth"))
+
+#TESTING PHASE
 model.eval()
+
+iou_test = []
+#here we just loop throught the test data and compute the Iou score
+with torch.no_grad():
+    for images, labels in test_dataloader:
+        images = [img.to(device) for img in images]
+        labels = [{k: v.to(device) for k, v in t.items()} for t in labels]
+
+        outputs = model(images)
+
+        for pred, target in zip(outputs, labels):
+            if len(pred['boxes']) == 0:
+                continue
+
+            best_index = pred['scores'].argmax()
+            pred_box = pred['boxes'][best_index].cpu().numpy()
+            true_box = target['boxes'][0].cpu().numpy()
+
+            iou = compute_iou(pred_box, true_box)
+            iou_test.append(iou)
+
+mean_iou = sum(iou_test) / len(iou_test)
+print(f"Test result IoU: {mean_iou:.4f}")
+
+#getting the last iou value for train and validation
+final_train_iou = iou_scores_train[-1]
+final_val_iou = iou_scores_val[-1]
+
+#saving the iou result of the training, validation (last step) and testing
+with open("results/fastercnn-{SAVE_NAME}.txt", "w") as f:
+    f.write(f"Final train IoU: {final_train_iou:.4f}\n")
+    f.write(f"Final validation   IoU: {final_val_iou:.4f}\n")
+    f.write(f"Final testing   IoU: {mean_iou:.4f}\n")
+
