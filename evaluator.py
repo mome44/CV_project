3424a146c2a43ca1d17b6bc5ad60b462cc4b1f93
CarlottaCCC <carlottaciani19@gmail.com>
2025-07-19 09:28:52 +0200

    Train and test PLDPR

---
+++ evaluator.py
@@ -0,0 +1,63 @@
+import torch
+import editdistance
+
+class Evaluator:
+    def __init__(self, idx2char, blank_index=0, use_levenshtein=True):
+        self.idx2char = idx2char
+        self.blank_index = blank_index
+        self.use_levenshtein = use_levenshtein
+        self.reset()
+
+    def reset(self):
+        self.total_chars = 0
+        self.correct_chars = 0
+        self.correct_seqs = 0
+        self.total_samples = 0
+        self.lev_dists = []
+
+    def _greedy_decode(self, logits):
+        # logits: [B, T, C]
+        preds = torch.argmax(logits, dim=-1)  # [B, T]
+        decoded = []
+
+        for seq in preds:
+            prev = self.blank_index
+            chars = []
+            for idx in seq:
+                idx = idx.item()
+                if idx != self.blank_index and idx != prev:
+                    chars.append(self.idx2char[idx])
+                prev = idx
+            decoded.append("".join(chars))
+        return decoded
+
+    def update(self, logits, target_strs):
+        # logits: [B, T, vocab_size]
+        pred_strs = self._greedy_decode(logits)
+
+        for pred, true in zip(pred_strs, target_strs):
+            self.total_samples += 1
+            self.total_chars += len(true)
+            correct = sum(p == t for p, t in zip(pred, true))
+            self.correct_chars += correct
+            if pred == true:
+                self.correct_seqs += 1
+            if self.use_levenshtein:
+                self.lev_dists.append(editdistance.eval(pred, true))
+
+    def compute(self):
+        char_acc = self.correct_chars / self.total_chars if self.total_chars > 0 else 0.0
+        seq_acc = self.correct_seqs / self.total_samples if self.total_samples > 0 else 0.0
+        lev_dist = sum(self.lev_dists) / self.total_samples if self.lev_dists else 0.0
+        return {
+            "char_accuracy": char_acc,
+            "seq_accuracy": seq_acc,
+            "avg_levenshtein": lev_dist if self.use_levenshtein else None
+        }
+
+    def print(self):
+        metrics = self.compute()
+        print(f"Character Accuracy:  {metrics['char_accuracy']:.4f}")
+        print(f"Sequence Accuracy:   {metrics['seq_accuracy']:.4f}")
+        if self.use_levenshtein:
+            print(f"Avg Levenshtein:     {metrics['avg_levenshtein']:.2f}")
---
+++ test_pdlpr.py
@@ -0,0 +1,121 @@
+import sys
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import os
+from igfe import IGFE
+from encoder import PDLPR_Encoder
+from decoder import ParallelDecoder
+from evaluator import Evaluator
+from train_pdlpr import update_vocab, build_vocab_from_label_folder, load_vocab
+from data import CCPDDataset
+import torch.nn as nn
+import torch.optim as optim
+import string
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from tqdm import tqdm
+import json
+import matplotlib.pyplot as plt
+
+
+def test(model_parts, evaluator, test_loader, char2idx, idx2char, device):
+    igfe, encoder, decoder = model_parts
+
+    # keep track of metrics for plot
+    test_seq_accs = []
+    test_char_accs = []
+    test_lev = []
+
+    igfe.eval()
+    encoder.eval()
+    decoder.eval()
+
+    evaluator = Evaluator(idx2char)
+    pbar = tqdm(test_loader, desc=f"Testing")
+
+    with torch.no_grad():
+        for batch in pbar:
+            images = batch["cropped_image"].to(device)
+            label_strs = batch["pdlpr_plate_string"]
+
+            unknown = set(c for s in label_strs for c in s if c not in char2idx)
+            if unknown:
+                char2idx, idx2char = update_vocab(unknown, char2idx, idx2char)
+                decoder.update_vocab_size(len(char2idx))
+
+
+            # Forward
+            features = igfe(images)
+            encoded = encoder(features)
+            logits = decoder(encoded)
+
+            evaluator.update(logits, label_strs)
+            metrics = evaluator.compute()
+            test_seq_accs.append(metrics["seq_accuracy"])
+            test_char_accs.append(metrics["char_accuracy"])
+            test_lev.append(metrics["avg_levenshtein"])
+
+
+    #saving the new vocabulary
+    with open("vocab.json", "w", encoding="utf-8") as f:
+        json.dump(char2idx, f, ensure_ascii=False, indent=2)
+
+    metrics = evaluator.compute()
+    evaluator.print()
+    return metrics, test_char_accs, test_seq_accs, test_lev
+
+
+# Standard transform
+transform = transforms.Compose([
+    transforms.Resize((48, 144)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
+])
+
+# Load data
+dataset = CCPDDataset(base_dir="dataset", transform=transform)
+_, _, test_loader = CCPDDataset.get_dataloaders(
+    base_dir="./dataset",
+    batch_size=16,
+    transform=transform
+)
+
+if os.path.exists('vocab.json'):
+    print("vocab.json found — loading...")
+    char2idx, idx2char = load_vocab('vocab.json')
+else: 
+    print("vocab.json not found — building it from labels...")
+    char2idx, idx2char = build_vocab_from_label_folder("dataset/labels_pdlpr/train", "vocab.json")
+
+vocab_size = len(char2idx)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+evaluator = Evaluator(idx2char)
+ctc_loss = nn.CTCLoss(blank=0)
+decoder_seq_len = 18  # From ParallelDecoder
+decoder = ParallelDecoder(dim=512, vocab_size=vocab_size, seq_len=decoder_seq_len).to(device).train()
+encoder = PDLPR_Encoder().to(device).train()
+igfe = IGFE().to(device).train()
+
+# load pre trained model if needed
+if os.path.exists( f'PLDPR/checkpoints/pdlpr_final.pt'):
+    print("checkpoint found. Loading state dict......")
+    checkpoint = torch.load( f'PLDPR/checkpoints/pdlpr_final.pt', map_location=device)
+    igfe.load_state_dict(checkpoint["igfe_state_dict"])
+    encoder.load_state_dict(checkpoint["encoder_state_dict"])
+    decoder.load_state_dict(checkpoint["decoder_state_dict"])
+    #optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
+else:
+    print("chackpoint not found. Please train the model first")
+
+print("Starting testing............")
+test_metrics, test_char_accs, test_seq_accs, test_lev = test(
+    model_parts=(igfe, encoder, decoder),
+    evaluator=evaluator,
+    test_loader=test_loader,
+    char2idx=char2idx,
+    idx2char=idx2char,
+    device=device
+)
